8cb73d24111fe07f5910c0937dd704268ee47644
Sergey Gurman <sgurman753@gmail.com>
2020-06-18 14:08:34 -0400

    made all the case end with s or not for years and months

--- task/creditcalc/creditcalc.py
+++ task/creditcalc/creditcalc.py
@@ -1,9 +1,9 @@
 from math import ceil, log
 
 """
-A = annuity payment
+A or a = annuity payment
 
-P = Credit principal.
+P or p = Credit principal.
 
 i = nominal (monthly) interest rate.
 
@@ -11,28 +11,28 @@ n = Number of payments. Usually, it’s the count of months.
 """
 
 
-def find_months(P, A, i):
+def find_months(p, a, i):
     """ Number of payments. Usually, it's the count of months."""
     i = i / 100
     i = i/(12 * 1)  # Given credit interest is divide by 12 months and 100%
-    n = log(A/(A - i * P), 1 + i)
+    n = log(a/(a - i * p), 1 + i)
     return ceil(n)
 
 
-def find_annuity_payment(P, n, i):
+def find_annuity_payment(p, n, i):
     i = i / 100
     nominator = i * pow(1 + i, n)
     denominator = pow(1 + i, n) - 1
-    A = P * nominator / denominator
-    return A
+    a = p * nominator / denominator
+    return a
 
 
-def find_principal(A, n, i):
+def find_principal(a, n, i):
     i = i / 100
     nominator = i * pow(1 + i, n)
     denominator = pow(1 + i, n) - 1
-    P = A / nominator / denominator
-    return P
+    a = a / nominator / denominator
+    return a
 
 
 print("What do you want to calculate:")
@@ -52,11 +52,24 @@ if choose == "n":
     years = months // 12
     months = months % 12
     if years == 0:
-        print(f"It takes {months} months to repay the credit")
+        if months == 1:
+            print(f"It takes {months} month to repay the credit")
+        else:
+            print(f"It takes {months} months to repay the credit")
     elif months == 0:
-        print(f"It takes {years} years to repay the credit")
+        if years == 1:
+            print(f"It takes {years} year to repay the credit")
+        else:
+            print(f"It takes {years} years to repay the credit")
     else:
-        print(f"It takes {years} years and {months} months to repay the credit")
+        if years == 1 and months == 1:
+            print(f"It takes {years} year and {months} month to repay the credit")
+        elif years > 1 and months == 1:
+            print(f"It takes {years} years and {months} month to repay the credit")
+        elif years == 1 and months > 1:
+            print(f"It takes {years} year and {months} months to repay the credit")
+        else:
+            print(f"It takes {years} years and {months} months to repay the credit")
 elif choose == "a":
     pass
 elif choose == "p":
